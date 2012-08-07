eb18951a6024ed4e78e52e4e2564f9313d749c94
Webonary <Webonary@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-07 09:34:26 +0000

    changed function for getting upload path
    git-svn-id: https://plugins.svn.wordpress.org/sil-dictionary-webonary/trunk@582580 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/xhtml-importer.php
+++ include/xhtml-importer.php
@@ -262,7 +262,7 @@ class sil_pathway_xhtml_Import extends WP_Importer {
 		if($extension == "css")
 		{
 			$upload_dir = wp_upload_dir();
-			$target_path = str_replace('http://' . $_SERVER['HTTP_HOST'], $_SERVER['DOCUMENT_ROOT'], $upload_dir['baseurl']);
+			$target_path = str_replace('http://' . $_SERVER['HTTP_HOST'], $_SERVER['DOCUMENT_ROOT'], '/' . get_blog_option($blog_id,'upload_path'));
 			$target_path = $target_path . "/imported-with-xhtml.css";
 			
 			//$from_path = $_SERVER['DOCUMENT_ROOT'] . "/wordpress/wp-content/uploads/" . date("Y") . "/" . date("m") . "/" . $filename;
