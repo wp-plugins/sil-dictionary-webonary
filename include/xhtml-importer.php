cd9a006d4833b78eaefb857a59515466176d9678
Webonary <Webonary@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-08 08:06:16 +0000

    changed path for pictures
    git-svn-id: https://plugins.svn.wordpress.org/sil-dictionary-webonary/trunk@583048 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/xhtml-importer.php
+++ include/xhtml-importer.php
@@ -197,8 +197,6 @@ class sil_pathway_xhtml_Import extends WP_Importer {
 				<input type="checkbox" name="chkConvertToLinks"> <?php esc_attr_e('Convert items into search links (semantic domains always convert to links).'); ?></input>
 			</div>
 			<p class="submit">
-				<?php $upload_dir = wp_upload_dir(); ?>
-				<input type="hidden" name="uploadpath" value="<?php echo $upload_dir['baseurl']; ?>">
 				<input type="submit" class="button" value="<?php esc_attr_e( 'Upload files and import' ); ?>" />
 			</p>
 			</form>
@@ -550,7 +548,7 @@ class sil_pathway_xhtml_Import extends WP_Importer {
 			
 			$newelement = $this->dom->createElement('a');
 			$newelement->appendChild($newimage);
-			$newelement->setAttribute("href", "javascript:openImage(\'" . $upload_dir['path'] . "/images/original/" . $pic . "\')");
+			$newelement->setAttribute("href", "javascript:openImage(\'/files/images/original/" . $pic . "\')");
 			$parent = $image->parentNode;			
 			$parent->replaceChild($newelement, $image);
 						
