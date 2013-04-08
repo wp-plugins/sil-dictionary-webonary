a9c8222b0eaeaa6bed82a56a0b285fcc540c250a
Webonary <Webonary@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-08 07:00:36 +0000

    version 2.1.1
    git-svn-id: https://plugins.svn.wordpress.org/sil-dictionary-webonary/trunk@693486 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sil-dictionary.php
+++ sil-dictionary.php
@@ -9,7 +9,7 @@ Author: SIL International
 Author URI: http://www.sil.org/
 Text Domain: sil_dictionary
 Domain Path: /lang/
-Version: v. 2.0.9
+Version: v. 2.1.1
 License: GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 */
 
@@ -40,7 +40,6 @@ define('SEARCHTABLE', $wpdb->prefix . 'sil_search');
 /*
  * Dependencies
  */
-
 // Infractstructure management: add and remove custom table(s) and custom taxonomies.
 require_once( dirname( __FILE__ ) . '/include/infrastructure.php' );
 // Code for searching on dictionaries.
