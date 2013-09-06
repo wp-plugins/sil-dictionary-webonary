4e1ea47b7b345cc4a1d656fa584fed0014b094d7
Webonary <Webonary@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-06 02:13:29 +0000

    version 3.0.1
    git-svn-id: https://plugins.svn.wordpress.org/sil-dictionary-webonary/trunk@767655 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/browseview_func.php
+++ include/browseview_func.php
@@ -32,7 +32,7 @@ function ajaxsearch()
 			*/
 				$display .= "<div class=post>" . $mypost->post_content . "</div>";
 				if( comments_open($mypost->ID) ) {
-					$display .= "<a href=\"" . $mypost->post_name. "\" rel=bookmark><u>Comments (" . get_comments_number($mypost->ID) . ")</u></a>"; 
+					$display .= "<a href=\"/" . $mypost->post_name. "\" rel=bookmark><u>Comments (" . get_comments_number($mypost->ID) . ")</u></a>"; 
 				}			
 			//}
 		}
