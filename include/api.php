55bbafe9b831825c2f257327d07540f8a6455b84
Webonary <Webonary@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-08 01:37:26 +0000

    4.0.4
    git-svn-id: https://plugins.svn.wordpress.org/sil-dictionary-webonary/trunk@1003551 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/api.php
+++ include/api.php
@@ -1,9 +1,9 @@
 <?php
 function webonary_api_init() {
-    global $webonary_api_mytype;
-        
-    $webonary_api_mytype = new Webonary_API_MyType();
-    add_filter( 'json_endpoints', array( $webonary_api_mytype, 'register_routes' ) );
+	global $webonary_api_mytype;
+
+	$webonary_api_mytype = new Webonary_API_MyType();
+	add_filter( 'json_endpoints', array( $webonary_api_mytype, 'register_routes' ) );
 }
 
 add_action( 'wp_json_server_before_serve', 'webonary_api_init' );
@@ -17,19 +17,53 @@ class Webonary_API_MyType {
         return $routes;
     }
 
-	public function import($data, $_headers){
+	public function import($_headers){
 		//array (for Collections) or an object (for Entities).
 		if(isset($_SERVER['PHP_AUTH_USER'])){
-			//$rettr = $data;
-			//sil_pathway_xhtml_Import::import_xhtml($data, true);
+
+			$arrDirectory = wp_upload_dir();
+			$uploadPath = $arrDirectory['path'];
+
+			$this->unzip($_FILES['filedata'], $uploadPath);
+
+			$zipPath = $uploadPath . "/" . str_replace(".zip", "", $_FILES['filedata']['name']);
+			$fileConfigured = $zipPath . "/configured.xhtml";
+			$xhtmlConfigured = file_get_contents($fileConfigured);
+
 			$import = new sil_pathway_xhtml_Import();
-			$import->import_xhtml($data, true);
+			$import->import_xhtml($xhtmlConfigured, true);
 			$import->index_searchstrings();
-			
+
 			//$rettr = add_action( 'init', 'import_xhtml' );
-								
-			//$rettr = "This will eventually display some useful information. Your credentials are USR: " . $_SERVER['PHP_AUTH_USER'] . " PASS: " . $_SERVER['PHP_AUTH_PW'];
+
 		}else{$rettr = "You are not logged in.";}
 		return array('returnedData'=>$rettr);
 	}
+
+	public function unzip($zipfile, $uploadPath)
+	{
+		$overrides = array( 'test_form' => false, 'test_type' => false );
+		$file = wp_handle_upload($zipfile, $overrides);
+
+		$zip = new ZipArchive;
+		$res = $zip->open($uploadPath . "/" . $zipfile['name']);
+		echo $res . "|";
+		if ($res === TRUE) {
+		  $unzip_success = $zip->extractTo($uploadPath);
+		  $zip->close();
+		  if($unzip_success)
+		  {
+			echo "zip file extracted successfully";
+		  }
+		  else
+		  {
+			echo "couldn't extract zip file to " . $uploadPath;
+		  }
+		} else {
+		  echo $zipfile['name'] . " isn't a valid zip file";
+		}
+
+		unlink($uploadPath . "/" . $zipfile['name']);
+	}
+
 }
